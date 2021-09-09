688685968ab49c24068fc46f825a55f70bd26c75
Kobi Cohen <kobic8@gmail.com>
2021-09-09 22:44:56 +0300

    final project for Dudi

--- ContactList.py
+++ ContactList.py
@@ -1,18 +1,44 @@
 import json
 
-filename = "dict_to_json.json"
-with open(filename) as f:
+
+def search_bf(txt, pat):
+    """ Bruteforce search of pattern in a given string """
+    len_pat = len(pat)
+    ind = 0
+    for ch in txt[:]:
+        if ch != pat[ind]:
+            ind = 0
+        else:
+            if ind == len_pat - 1:  # the whole pattern was scanned and found in txt
+                return True
+            else:
+                ind += 1
+    return False
+
+
+def main():
+    """ The program loads a contact list as a json file (dict_to_json.json), asks from the user names to locate and
+    retrieves the matched contacts """
+
+    filename = "dict_to_json.json"
+    with open(filename) as f:
         all_contacts = json.load(f)
-print(all_contacts.keys())
+    print(all_contacts.keys())
 
-user_input = input("Enter something:")
+    user_input = input("Enter something:")
+    while not user_input.lower() == 'q':
+        if type(user_input) == int:  # we iterate through values
+            user_input = str(user_input)
 
-if type(user_input) == int:  # we iterate through values
-    user_input = str(user_input)
+        matched = {}
+        for key, value in all_contacts.items():
+            if search_bf(key.lower(), user_input.lower()) or search_bf(value, user_input):
+                matched[key] = value
 
-matched = {}
-for key, value in all_contacts.items():
-    if user_input.lower() in key.lower() or user_input in value:
-        matched[key] = value
+        print("The following contacts found matched:\n {}".format(matched))
+        user_input = input("Find another contact, or refine the search. Press q to exit:")
+    print("Thank you")
 
-print("The following contacts found matched:\n {}".format(matched))
+
+if __name__ == '__main__':
+    main()
